943703e34e0f99ad7363638064ed605a6e2f3496
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-06 22:40:42 +0000

    trying to make the js work

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,12 @@
-const vampire = () => console.log("HELLO WORLD");
+const vampire = (text) => console.log(`Hello ${text}`);
 
-$(document).ready(function) {
-  vampire;
-}
+// $(document).ready(function(){
+// document.on("doubleclick",console.log(this) )
+// });
+
+document.addEventListener("doubleclick",() => {
+  console.log("FUCK THIS")
+})
+
+// textarea.on("doubleclick",vampire(1) );
+// textarea.on("click",vampire(2) )
