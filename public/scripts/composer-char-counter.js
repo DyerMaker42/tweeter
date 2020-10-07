60f8a4208f08a30dbaf426e48e2eaa2978cb9f76
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-07 01:04:43 +0000

    left notes to self in comments to come back to

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     const input = $(this);
     console.log(input.val().length);
     const counterNumber = 140 - input.val().length
+    //research another way to do this later
     $(".counter").text(counterNumber);
     if (counterNumber <= 0) {
       $(".counter").css("color", '#f76e6e')
