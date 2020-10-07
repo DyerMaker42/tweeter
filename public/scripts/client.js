b46e58e967ce9e9067967ac1892f925024aa9ba3
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-10-07 22:32:57 +0000

    added driver code to client js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,19 @@
  */
 
 $(document).ready(function () {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+  
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
 
 
 
